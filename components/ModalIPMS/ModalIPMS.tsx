65af36f7a90db5118fdafc639625921f57c89098
Akihiro Kato <4kihiro.kato@gmail.com>
2023-08-29 04:39:34 +0900

    add ipms image

--- components/ModalIPMS/ModalIPMS.tsx
+++ components/ModalIPMS/ModalIPMS.tsx
@@ -1,10 +1,20 @@
-import { Modal } from '@mantine/core'
+import { Modal, CloseButton } from '@mantine/core'
+import classes from './ModalIPMS.module.css'
+import Image from 'next/image'
+import IPMSImage from './ipms.png'
+
 
 export const ModalIPMS = ({opened, close}:{opened: boolean, close: () => void}) => {
   return (
-    <Modal opened={opened} onClose={close}>
-      <div>
-        modal IPMS
+    <Modal
+      opened={opened}
+      onClose={close}
+      size="100%"
+      withCloseButton={false}
+    >
+      <CloseButton size="xl" />
+      <div className={classes.image_wrap}>
+        <Image src={IPMSImage} alt="IPMS" />
       </div>
     </Modal>
   )
